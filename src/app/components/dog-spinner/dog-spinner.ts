06139b8ada1a42343a6549a142d0f295f947e47f
gabor6502 <elan.gabor@gmail.com>
2025-07-18 17:37:05 -0500

    added spinner and high scores area, layout changes

---
+++ src/app/components/dog-spinner/dog-spinner.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dog-spinner',
+  imports: [],
+  templateUrl:'./dog-spinner.html',
+  styleUrl: './dog-spinner.scss'
+})
+export class DogSpinner 
+{
+
+}
---
+++ src/app/components/high-scores/high-scores.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-high-scores',
+  imports: [],
+  templateUrl: './high-scores.html',
+  styleUrl: './high-scores.scss'
+})
+export class HighScores {
+
+}
