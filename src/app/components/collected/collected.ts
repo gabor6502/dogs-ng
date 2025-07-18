2946baa4009b99fc704a3c0075b1aaded6c059a7
gabor6502 <elan.gabor@gmail.com>
2025-07-17 22:05:05 -0500

    shaky hands

---
+++ src/app/components/collected/collected.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-collected',
+  imports: [],
+  templateUrl: './collected.html',
+  styleUrl: './collected.scss'
+})
+export class Collected {
+
+}
---
+++ src/app/components/dogs-collected/dogs-collected.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dogs-collected',
+  imports: [],
+  templateUrl: './dogs-collected.html',
+  styleUrl: './dogs-collected.scss'
+})
+export class DogsCollected {
+
+}
