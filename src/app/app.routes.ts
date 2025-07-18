e33e3db9ae246d266d9316693dc4717b6697be62
gabor6502 <elan.gabor@gmail.com>
2025-07-17 22:03:53 -0500

    a lot of setup done...nothing is final

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = 
+[
+    {
+        path: '',
+        pathMatch: 'full',
+        loadComponent: () => {return import('./components/home/home').then((module) => module.Home)}
+    },
+    {
+        path: 'collected',
+        loadComponent: () => {return import('./components/collected/collected').then((module) => module.Collected)}
+    }
+];
