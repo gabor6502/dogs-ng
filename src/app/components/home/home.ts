54cd2050041eeb08ca169222972086c18a4f75bc
gabor6502 <elan.gabor@gmail.com>
2025-07-18 17:37:30 -0500

    quick change to home

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { DogSpinner } from "../dog-spinner/dog-spinner";
+import { HighScores } from "../high-scores/high-scores";
+import { Collected } from "../collected/collected";
 
 @Component({
   selector: 'app-home',
-  imports: [DogSpinner],
+  imports: [DogSpinner, HighScores, Collected],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
