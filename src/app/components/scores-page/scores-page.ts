0bcfbcbbc0b17c5c8af89d7cf2a7df8f0370e775
gabor6502 <elan.gabor@gmail.com>
2025-07-29 22:31:43 -0500

    started scores page

--- src/app/components/scores-page/scores-page.ts
+++ src/app/components/scores-page/scores-page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Score } from '../../models/score.type'
 
 @Component({
   selector: 'app-scores-page',
@@ -8,5 +9,14 @@ import { Component } from '@angular/core';
 })
 export class ScoresPage 
 {
-  // empty
+  // temp fake scores
+  scores = signal<Score[]>([ 
+    {name: "BigRig2117", score: 132342, date: "2022/01/07"},
+    {name: "LionelMessi#1", score: 22314, date: "2024/04/30"},
+    {name: "JRazz", score: 9859348, date: "2020/11/23"},
+    {name: "ColloqiuallyKnown", score: 202123, date: "2005/03/12"},
+    {name: "UngaBunga", score: 777777777, date: "2018/09/25"}
+  ])
+
+  
 }
