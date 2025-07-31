c96a592b01181b252a716d3e4dfca943a65d6347
gabor6502 <elan.gabor@gmail.com>
2025-07-30 20:52:51 -0500

    thoughts on the collected dogs section

--- src/app/components/collected/collected.ts
+++ src/app/components/collected/collected.ts
@@ -6,6 +6,10 @@ import { Component } from '@angular/core';
   templateUrl: './collected.html',
   styleUrl: './collected.scss'
 })
-export class Collected {
+export class Collected 
+{
+  // empty
 
+  // I think that the spinner and collected must be combined into one component
+  // this way they can acess each other's data better, in a more straightforward, less convoluted way
 }
